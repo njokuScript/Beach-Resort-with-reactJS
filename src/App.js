3a8e11226c818dd50d4db19441ece813bf204786
Njoku Emmanuel <kalunjoku123@gmail.com>
2019-07-07 19:33:32 +0100

    created navbar.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
 
 import { Route, Switch } from "react-router-dom";
-
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms/" component={Rooms} />
