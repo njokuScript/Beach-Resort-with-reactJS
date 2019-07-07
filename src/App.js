63f1c915ed42a3b4a6e60258177eb850739d1b74
Njoku Emmanuel <kalunjoku123@gmail.com>
2019-07-07 19:28:43 +0100

    set up react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import "./App.css";
 import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
-import Error from "./pages/error";
+import Error from "./pages/Error";
+
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Home />
-      <Rooms />
-      <SingleRoom />
-      <Error />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 }
