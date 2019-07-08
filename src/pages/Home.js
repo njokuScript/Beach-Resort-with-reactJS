cddc34eae92e05e69fe4e3de25d0a1dd902f6706
Njoku Emmanuel <kalunjoku123@gmail.com>
2019-07-08 08:22:54 +0100

    created and styled the navabar and hero component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Hero from "../components/Hero";
 export default function Home() {
-  return <div>hello from home page</div>;
+  return <Hero />;
 }
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,5 +1,8 @@
 import React from "react";
+import Hero from "../components/Hero";
 
-export default function Rooms() {
-  return <div>hello from rooms page</div>;
-}
+const Rooms = () => {
+  return <Hero hero="roomsHero" />;
+};
+
+export default Rooms;
